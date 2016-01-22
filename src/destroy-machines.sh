06f95714b4df8d25dc3d01368f3aa8298255cc9b
Petar Smilajkov <peconi@peconi.com>
2016-01-22 10:33:45 -0500

    Updated destroy-machines script - switched to shflags and fixed some bugs

--- src/destroy-machines.sh
+++ src/destroy-machines.sh
@@ -7,44 +7,41 @@
 ## ==================================
 ## Updates:
 ## - 12/16/2015 - expanded the initial version - Petar Smilajkov
+## - 01/22/2016 - switched to shflags and fixed some issues
 ## ================================================================
+
+# source shflags
+source @BASHLIBS@/shflags
 
 # set fonts for help highlights
 NORM=`tput sgr0`
 BOLD=`tput bold`
 REV=`tput smso`
 
-function show_help {
-  cat << EOF
-Usage: ${BOLD}destroy-machines${NORM} [-n|--name | -q|--quantity]
+DEFINE_string 'name' 'dh' 'full name (or suffix before 000X) of the docker host to remove' 'n'
+DEFINE_string 'provider' 'vb' 'provider identifier (vb=virtual box, do=digital ocean, ssh=generic, etc.)' 'p'
+DEFINE_string 'quantity' '1' 'number of docker hosts to delete (will start from 0001, up to number you specify)' 'q'
 
-Example: ${REV}destroy-machines -n dh.do.nyc3 -q 3${NORM}
--> Destroys a specified -q NUMBER of Docker Hosts prefixed with -n dockerhost.do.nyc3 name.
--> I.e. 0001.dockerhost.do.nyc3, 0002.dockerhost.do.nyc3, and 0003.dockerhost.do.nyc3
+# parse the command-line
+FLAGS "$@" || exit 1
+eval set -- "${FLAGS_ARGV}"
 
-Example: ${REV}destroy-machines -n 0001.dockerhost.do.nyc3${NORM}
--> Destroys 0001.dockerhost.do.nyc3 machine (single machine w/ full name as specified)
-
-        -h|--help               displays this help
-
-        -n|--name NAME          [REQUIRED] full name of the docker host to remove
-                                ex: 0001.dh.do.nyc3 (for removing a single machine)
-				ex2: dh.do.nyc (w/o #) if used with -q switch (deletes > 1 machine)
-
-	-q|--quantity NUMBER	[OPTIONAL] number of docker hosts to delete 
-                                -> will start from 0001 and go UP to NUMBER specified (inclusive)
-                                ex: 3 -> this will delete hosts 0001.NAME, 0002.NAME and 0003.NAME
-				if not provided, only docker host specified with -n NAME will be deleted
-
-EOF
-}
+# if help - just exit
+if [ "${FLAGS_help}" -eq ${FLAGS_TRUE} ]; then exit 1; fi
 
 function destroy_machines {
   local NAME=$1
-  local QUANTITY=$2
+  local PROVIDER=$2
+  local QUANTITY=$3
+
+  if [ "$PROVIDER" == "vb" ]; then
+    SUFFIX=".local"
+  else
+    SUFFIX=""
+  fi
 
   if [[ $QUANTITY -eq 1 ]]; then
-    destroy_machine $NAME
+    destroy_machine "0001".$NAME.$PROVIDER$SUFFIX
   else
     for ((a=1; a <= QUANTITY ; a++)) do
       PREFIX=""
@@ -67,7 +64,7 @@ function destroy_machines {
           ;;
       esac
 
-      destroy_machine "$PREFIX.$NAME"
+      destroy_machine "$PREFIX.$NAME.$PROVIDER$SUFFIX"
     done
   fi  
 }
@@ -78,37 +75,26 @@ function destroy_machine {
   docker-machine rm "$NAME" &
 }
 
-NUMARGS=$#
-if [ $NUMARGS -eq 0 ]; then
-  show_help
-fi
-
 # init vars to default values
 NAME=""
-PROVIDER="do"
+PROVIDER="vb"
 QUANTITY=0
 
-while getopts "h help n::name::p:provider:q:quantity:" opt; do
-  case $opt in
-    n|name)
-      NAME=$OPTARG
-      ;;
-    p|provider)
-      PROVIDER=$OPTARG
-      ;;
-    q|quantity)
-      QUANTITY=$OPTARG
-      ;;
-    h|help)
-      show_help
-      exit 1;
-      ;;
-    \?)
-      show_help
-      exit 1
-      ;;
-  esac
-  shift $((OPTIND-1))
-done
+echo ""
+echo "FLAGS:"
+echo "- Name    : ${FLAGS_name}"
+echo "- Provider: ${FLAGS_provider}"
+echo "- Quantity: ${FLAGS_quantity}"
 
-destroy_machines $NAME $QUANTITY
+# update variables with submitted data
+if [[ "${FLAGS_name}" ]]; then NAME="${FLAGS_name}"; fi
+if [[ "${FLAGS_provider}" ]]; then PROVIDER="${FLAGS_provider}"; fi
+if [[ "${FLAGS_quantity}" ]]; then QUANTITY="${FLAGS_quantity}"; fi
+
+echo ""
+echo "New Vars:"
+echo "- Name    : $NAME"
+echo "- Provider: $PROVIDER"
+echo "- Quantity: $QUANTITY"
+
+destroy_machines $NAME $PROVIDER $QUANTITY
